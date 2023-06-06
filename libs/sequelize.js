9660f2f7813fd50fa3e6de275a71c44d54a7b654
Mateo Zarate <mateozaratefw@gmail.com>
2023-06-05 23:37:49 -0300

    Primer commit

---
+++ libs/sequelize.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+
+const { config } = require("../config/config");
+const setupModels = require("../db/models/index");
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+const sequelize = new Sequelize(URI, { dialect: "postgres", logging: true }); // Se crea una instancia de Sequelize, ya gestiona el pooling.
+
+setupModels(sequelize);
+
+sequelize.sync();
+
+module.exports = sequelize;
--- services/product.service.js
+++ services/product.service.js
@@ -1,9 +1,10 @@
-const { faker } = require('@faker-js/faker');
-const boom = require('@hapi/boom');
+const { faker } = require("@faker-js/faker");
+const boom = require("@hapi/boom");
+
+const sequelize = require("../libs/sequelize");
 
 class ProductsService {
-
-  constructor(){
+  constructor() {
     this.products = [];
     this.generate();
   }
@@ -16,7 +17,7 @@ class ProductsService {
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.imageUrl(),
-        isBlock: faker.datatype.boolean(),
+        isBlock: faker.datatype.boolean()
       });
     }
   }
@@ -25,30 +26,32 @@ class ProductsService {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data
-    }
+    };
     this.products.push(newProduct);
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    const query = "SELECT * FROM task";
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
-    const product = this.products.find(item => item.id === id);
+    const product = this.products.find((item) => item.id === id);
     if (!product) {
-      throw boom.notFound('product not found');
+      throw boom.notFound("product not found");
     }
     if (product.isBlock) {
-      throw boom.conflict('product is block');
+      throw boom.conflict("product is block");
     }
     return product;
   }
 
   async update(id, changes) {
-    const index = this.products.findIndex(item => item.id === id);
+    const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
-      throw boom.notFound('product not found');
+      throw boom.notFound("product not found");
     }
     const product = this.products[index];
     this.products[index] = {
@@ -59,14 +62,13 @@ class ProductsService {
   }
 
   async delete(id) {
-    const index = this.products.findIndex(item => item.id === id);
+    const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
-      throw boom.notFound('product not found');
+      throw boom.notFound("product not found");
     }
     this.products.splice(index, 1);
     return { id };
   }
-
 }
 
 module.exports = ProductsService;
