1568abc1d2924a3425517066d6b930d09aa2ea73
Mateo Zarate <mateozaratefw@gmail.com>
2023-06-08 17:50:36 -0300

    Todo funcionando con POSTGRES

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -1,5 +1,7 @@
-const { Model, DataTypes, Sequelize } = require("sequelize");
-const USER_TABLE = "users";
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const USER_TABLE = 'users';
+
 const UserSchema = {
   id: {
     allowNull: false,
@@ -10,30 +12,43 @@ const UserSchema = {
   email: {
     allowNull: false,
     type: DataTypes.STRING,
-    unique: true
+    unique: true,
   },
   password: {
     allowNull: false,
     type: DataTypes.STRING
   },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'customer'
+  },
   createdAt: {
     allowNull: false,
     type: DataTypes.DATE,
-    field: "create_at",
+    field: 'create_at',
     defaultValue: Sequelize.NOW
   }
-};
+}
 
 class User extends Model {
-  static associate() {}
+  static associate(models) {
+    this.hasOne(models.Customer, {
+      as: 'customer',
+      foreignKey: 'userId'
+    });
+  }
+
   static config(sequelize) {
     return {
       sequelize,
       tableName: USER_TABLE,
-      modelName: "User",
+      modelName: 'User',
       timestamps: false
-    };
+    }
   }
 }
-module.exports = { USER_TABLE, UserSchema, User };
+
+
+module.exports = { USER_TABLE, UserSchema, User }
 
--- libs/postgres.js
+++ libs/postgres.js
@@ -4,7 +4,7 @@ async function getConnection() {
   const client = new Client({
     host: 'localhost',
     port: 5432,
-    user: 'nico',
+    user: 'Maradona',
     password: 'admin123',
     database: 'my_store'
   });
